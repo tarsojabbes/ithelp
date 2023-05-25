36b6751fbaae50576ebea5394488638bbd1a5716
Tarso Jabbes <tarsojabbes@gmail.com>
2023-05-25 18:47:29 -0300

    Resolve erros e inclui busca com email e avaliacao de analista

--- Logico/Controller/AnalistaController.pl
+++ Logico/Controller/AnalistaController.pl
@@ -12,7 +12,7 @@ lerJSON(FilePath, File) :-
 
 % Formata a linha que será adicionada no arquivo JSON
 analistaToJSON(Id, Nome, Email, Senha, Avaliacao, AnalistaJSON) :-
-    swritef(AnalistaJSON, '{"id": "%w", "nome": "%w", "email": "%w", "senha": "%w", "avaliacao": "%w"}',
+    swritef(AnalistaJSON, '{"id": %w, "nome": "%w", "email": "%w", "senha": "%w", "avaliacao": %w}',
             [Id, Nome, Email, Senha, Avaliacao]).
 
 % Exibir um analista
@@ -21,7 +21,7 @@ exibirAnalista([H|T]) :-
     write("ID: "), writeln(H.id),
     write("Nome: "), writeln(H.nome),
     write("Email: "), writeln(H.email),
-    write('Avaliacao: '), writeln(H.avaliacao)
+    write('Avaliacao: '), writeln(H.avaliacao).
 
 exibirAnalistas() :-
     lerJSON("./banco/analistas.json", Analistas),
@@ -47,18 +47,18 @@ salvarAnalista(Nome, Email, Senha, Avaliacao) :-
 % Remove um analista no JSON
 removerAnalistaJSON([], _, []).
 removerAnalistaJSON([H|T], H.id, T).
-removerAnalistaJSONO([H|T], Id, [H|Out]) :- removerAgenteJSON(T, Id, Out).
+removerAnalistaJSON([H|T], Id, [H|Out]) :- removerAnalistaJSON(T, Id, Out).
 
 % Remove um analista
 removerAnalista(Id) :-
     lerJSON("./banco/analistas.json", File),
-    removerAgenteJSON(File, Id, SaidaParcial),
+    removerAnalistaJSON(File, Id, SaidaParcial),
     analistasToJSON(SaidaParcial, Saida),
     open("./banco/analistas.json", write, Stream), write(Stream, Saida), close(Stream).
 
 % Busca um analista pelo ID no JSON
 buscarAnalistaPorIdJSON([], _, null).
-buscarAnalistaPorIdJSON([Agente|_], Agente.id, Agente).
+buscarAnalistaPorIdJSON([Analista|_], Analista.id, Analista).
 buscarAnalistaPorIdJSON([_|T], Id, [_|Out]) :- buscarAnalistaPorIdJSON(T, Id, Out).
 
 % Busca um analista pelo ID 
@@ -66,7 +66,32 @@ buscarAnalistaPorId(Id, Analista) :-
     lerJSON("./banco/analistas.json", File),
     buscarAnalistaPorIdJSON(File, Id, Analista).
 
+% Busca um analista pelo Email no JSON
+buscarAnalistaPorEmailJSON([], _, null).
+buscarAnalistaPorEmailJSON([Analista|_], Analista.email, Analista).
+buscarAnalistaPorEmailJSON([_|T], Email, [_|Out]) :- buscarAnalistaPorEmailJSON(T, Email, Out).
+
 % Busca um analista pelo Email
+buscarAnalistaPorEmail(Email, Analista) :-
+    lerJSON("./banco/analistas.json", File),
+    buscarAnalistaPorEmailJSON(File, Email, Analista).
+
+% Atualiza a avaliacao de um analista no JSON
+editarAvaliacaoAnalistaJSON([], _, _, []).
+editarAvaliacaoAnalistaJSON([H|T], H.id, Avaliacao, [_{id: H.id, avaliacao: Avaliacao, nome: H.nome, email: H.email, senha: H.senha}|T]).
+editarAvaliacaoAnalistaJSON([H|T], Id, Avaliacao, [H|Out]) :-
+    editarAvaliacaoAnalistaJSON(T, Id, Avaliacao, Out).
 
 % Atualiza a avaliacao de um analista
+editarAvaliacaoAnalista(Id, NovaNota) :-
+    lerJSON("./banco/analistas.json", File),
+    buscarAnalistaPorId(Id, AnalistaEncontrado),
+    calculaNovaAvaliacao(AnalistaEncontrado.avaliacao, NovaNota, AvaliacaoNova),
+    editarAvaliacaoAnalistaJSON(File, Id, AvaliacaoNova, SaidaParcial),
+    analistasToJSON(SaidaParcial, Saida),
+    open("./banco/analistas.json", write, Stream), write(Stream, Saida), close(Stream).
+
+% Calcula nova avaliacao
+calculaNovaAvaliacao(AvaliacaoAntiga, NovaNota, AvaliacaoNova) :-
+    AvaliacaoNova is (integer(AvaliacaoAntiga)+integer(NovaNota))/2.
 
